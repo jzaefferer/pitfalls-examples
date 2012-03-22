a3af10929b940ac282bb21456fbdb60d248ef54c
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-03-22 15:28:21 +0100

    Refactor gallery, inline storeScroll method

--- app/gallery/gallery.js
+++ app/gallery/gallery.js
@@ -18,17 +18,6 @@ SimpleHistory.start(function(url, state) {
 	$("#status").text("state changed");
 });
 
-function storeScroll() {
-	var parts = URL.parse(location.href);
-	if (parts.path !== "/") {
-		return;
-	}
-	var scrollTop = $(document).scrollTop();
-	SimpleHistory.replaceState(location.pathname, {
-		scroll: scrollTop
-	});
-}
-
 $("a:not([href^=http])").click(function(event) {
 	if (event.metaKey || event.shiftKey || event.ctrlKey) {
 		return;
@@ -37,4 +26,13 @@ $("a:not([href^=http])").click(function(event) {
 	SimpleHistory.pushState(this.href);
 });
 
-$(document).scroll(storeScroll.debounce());
+$(document).scroll(function() {
+	var parts = URL.parse(location.href);
+	if (parts.path !== "/") {
+		return;
+	}
+	var scrollTop = $(document).scrollTop();
+	SimpleHistory.replaceState(location.pathname, {
+		scroll: scrollTop
+	});
+}.debounce());
